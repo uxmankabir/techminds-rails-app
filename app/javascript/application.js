0f22043dc090083d2518f4d808b1926d1b03b4f7
Usman Kabir <usman.kabir.k@gmail.com>
2023-05-21 04:45:18 +0500

    created static portfolio website

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  centerInsufficientSlides: true,
+
+  slidePrevClass: 'swiper-button__prev',
+  slideNextClass: 'swiper-button__next',
+
+  navigation: {
+    nextEl: '.swiper-buttons__next',
+    prevEl: '.swiper-buttons__prev',
+  },
+});
