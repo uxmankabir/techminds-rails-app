70d25cbff3e41f2418b4156dada48d4ab7b6370d
Usman Kabir <usman.kabir.k@gmail.com>
2023-05-27 05:50:47 +0500

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,79 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password')
+end
+
+home = Home.create!(title: 'Rem nihil molestiae',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate v elit esse cillum dolore eu fugiat nulla pariatur.')
+
+home.image.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/Mask group.png')),
+  filename: 'Mask group.png'
+)
+
+about = About.create(
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate v elit esse cillum dolore eu fugiat nulla pariatur.',
+  project_completed_count: 780,
+  happy_clients_count: 220,
+  title_one: 'Web Development',
+  description_one: '220+ Web Completed',
+  title_two: 'App Development',
+  description_two: '360+ App Completed',
+  title_three: 'SEO',
+  description_three: '320+ Web Completed',
+  title_four: 'Digital Marketing',
+  description_four: 'yourmail@gmail.com'
+)
+about.icon_one.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/web-dev-sm.png')),
+  filename: 'web-dev-sm.png'
+)
+about.icon_two.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/app-dev-sm.png')),
+  filename: 'app-dev-sm.png'
+)
+about.icon_three.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/digital-marketing-sm.png')),
+  filename: 'digital-marketing-sm.png'
+)
+about.icon_four.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/seo-sm.png')),
+  filename: 'seo-sm.png'
+)
+about.diamond_icon.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/diamond.svg')),
+  filename: 'diamond.svg'
+)
+
+service = Service.create(description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.')
+
+service_item = ServiceItem.create(title: 'MOBILE APP DEVELOPMENT',
+                                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing incididunt ut labore.', service:)
+service_item.icon.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/app-dev-lg.svg')),
+  filename: 'app-dev-lg.svg'
+)
+
+portfolio = Portfolio.create(description: 'Perfect solution for digital experience')
+
+project = PortfolioItem.create(title: 'DIGITAL MARKETING', description: 'Transforming & Modeling Data',
+                               portfolio:)
+project.image.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/project-1.png')),
+  filename: 'project-1.png'
+)
+
+team = Team.create(description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut.')
+
+member = Member.create(name: 'Lorem ipsum', role: 'Manager', location: 'Lahore, Pakistan', team:)
+
+member.image.attach(
+  io: File.open(File.join(Rails.root, 'app/assets/images/member-1.png')),
+  filename: 'member-1.png'
+)
+
+Contact.create(location: '47 Abdussalam street Maple ON, L6A3A8 Canada', email: 'yourmail@gmail.com',
+               phone: '+00 111 222 3333', latitude: '31.48109041354171', longitude: '74.31495667033114')
